a35fec442e1293408763a7ff4bfdabfbed096339
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-06-09 15:10:17 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -13,4 +13,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
