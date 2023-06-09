a5390059501f239d51c2ecf1cf422322aed870e1
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-06-09 14:52:29 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+from pyfiglet import Figlet
+
+app = Flask(__name__)
+f = Figlet(font='doom')  # You can choose other fonts too
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        text = request.form['text']
+        ascii_art = f.renderText(text)
+        return render_template('index.html', ascii_art=ascii_art)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
